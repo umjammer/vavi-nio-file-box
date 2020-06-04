a69046fc87154085e0c943af1c185cba7f4cc278
umjammer <umjammer@gmail.com>
2020-06-04 11:14:50 +0900

    make those fuse compliant

--- BoxFS.java
+++ BoxFS.java
@@ -50,6 +50,6 @@ public class BoxFS {
 
         FileSystem fs = new BoxFileSystemProvider().newFileSystem(uri, env);
 
-        Fuse.Factory.getFuse().mount(fs, args[0], Collections.EMPTY_MAP);
+        Fuse.getFuse().mount(fs, args[0], Collections.EMPTY_MAP);
     }
 }
